e91b46cd637e5f5ab4054ad8414d4f02b0b967cf
Hisown001 <2300724681@mubs.ac.ug>
2025-02-05 10:29:48 +0300

    Add files via upload

---
+++ WAD Project/main.js
@@ -0,0 +1,30 @@
+//regular array
+let students =['davis','michelle','cedra'];
+
+//pop up
+students.pop();
+console.log(students);
+
+//push
+students.push('noah','solomon');
+console.log(students);
+
+//shift
+students.shift();
+console.log(students);
+
+//unshift
+students.unshift('john','trevor');
+console.log(students);
+
+//slice
+students.slice('mark','william');
+console.log(students);
+
+//associative arrays
+let items={
+    "fruit":'apple',
+    "vegetable":'nakati',
+    "other item": 'mango',
+};
+console.log(items.fruit);
